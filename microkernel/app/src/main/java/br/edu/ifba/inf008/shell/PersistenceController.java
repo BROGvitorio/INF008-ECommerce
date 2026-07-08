64705663a7a29ec25bf204c6407801c91ab5185a
Melissa Vitorio <melissavitorio.me@gmail.com>
2026-07-08 10:50:48 -0300

    refactor: simplify PersistenceController to accept an entity

--- microkernel/app/src/main/java/br/edu/ifba/inf008/shell/PersistenceController.java
+++ microkernel/app/src/main/java/br/edu/ifba/inf008/shell/PersistenceController.java
@@ -1,67 +1,80 @@
 package br.edu.ifba.inf008.shell;
 
 import br.edu.ifba.inf008.database.HibernateUtil;
-import br.edu.ifba.inf008.interfaces.IEntityConvertible;
 import br.edu.ifba.inf008.interfaces.IPersistenceController;
 
 import org.hibernate.Session;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class PersistenceController implements IPersistenceController {
 
-    public void save(IEntityConvertible record) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            Object entity = record.toHibernateEntity();
+    private void checkEntity (Object record) {
+        if (!record.getClass()
+            .isAnnotationPresent(jakarta.persistence.Entity.class))
+            throw new IllegalArgumentException("This object isn't a Entity.");
+    }
+
+    public void save(Object entity) {
+        checkEntity(entity);
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) 
+        {
             session.beginTransaction();
             session.persist(entity);
             session.getTransaction().commit();
-        } catch (Exception e) {
-            if (session.getTransaction().isActive())
-                session.getTransaction().rollback();
-            session.close();
-
+        } catch (Exception e) 
+        {
+            throw new RuntimeException("Erro ao salvar entidade: " + e.getMessage(), e);
         }
     }
 
-    public void update(IEntityConvertible record) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            Object entity = record.toHibernateEntity();
+    public void update(Object entity) {
+        checkEntity(entity);
 
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) 
+        {
             session.beginTransaction();
             session.merge(entity);
             session.getTransaction().commit();
-        } catch (Exception e) {
-            if (session.getTransaction().isActive())
-                session.getTransaction().rollback();
+        } catch (Exception e) 
+        {
+            throw new RuntimeException("Erro ao atualizar entidade: " + e.getMessage(), e);
         }
     }
 
-    public void delete(IEntityConvertible record) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        try {
-            Object entity = record.toHibernateEntity();
-
+    public void delete(Class<?> clazz, Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) 
+        {
             session.beginTransaction();
+            
+            Object entity = session.find(clazz, id);
+            if (entity == null) {
+                throw new NoSuchElementException(clazz.getSimpleName() + " com id " + id + " não encontrado.");
+            }
+
             session.remove(entity);
             session.getTransaction().commit();
-        } catch (Exception e) {
-            if (session.getTransaction().isActive())
-                session.getTransaction().rollback();
+        } catch (Exception e) 
+        {
+            throw new RuntimeException("Erro ao deletar entidade: " + e.getMessage(), e);
         }
     }
 
     public <T> T findById(Class<T> clazz, Long id) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        return session.find(clazz, id);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) 
+        {
+            return session.find(clazz, id);
+        }
     }
 
-    public <T> List<T> findAll(String tableName, Class<T> clazz) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        String query = "FROM " + tableName;
-        return session.createQuery(query, clazz).list();
+    public <T> List<T> findAll(Class<T> clazz) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession())
+        {
+            String query = "FROM " + clazz.getSimpleName().toLowerCase() + "s";
+            return session.createQuery(query, clazz).list();
+        }
     }
 
 }
