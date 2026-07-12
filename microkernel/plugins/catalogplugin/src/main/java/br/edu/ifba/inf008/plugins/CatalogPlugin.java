4414b3eeafeb65ead395ded6f95cfb8e3eb875aa
VanessaVilas <vanessavilas57@gmail.com>
2026-07-12 18:52:48 -0300

    Product Registration

--- microkernel/plugins/catalogplugin/src/main/java/br/edu/ifba/inf008/plugins/CatalogPlugin.java
+++ microkernel/plugins/catalogplugin/src/main/java/br/edu/ifba/inf008/plugins/CatalogPlugin.java
@@ -1,83 +1,12 @@
 package br.edu.ifba.inf008.plugins;
 
 import br.edu.ifba.inf008.interfaces.IPlugin;
-import br.edu.ifba.inf008.interfaces.ICore;
-import br.edu.ifba.inf008.interfaces.IUIController;
-import br.edu.ifba.inf008.interfaces.IPersistenceController;
-import br.edu.ifba.inf008.domain.Product;
-
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.geometry.Insets;
-import javafx.scene.text.Text;
-import javafx.geometry.Pos;
-import java.util.List;
 
 public class CatalogPlugin implements IPlugin
 {
-    private void ProductListing() {
-        FlowPane catalog = new FlowPane();
-        catalog.setHgap(15);
-        catalog.setVgap(15);
-        catalog.setPadding(new Insets(15));
-
-        Label title = new Label("Catálogo de Produtos");
-        title.setStyle("-fx-font-size: 24px;" + "-fx-font-weight: bold;");
-
-        IPersistenceController persistenceController = ICore.getInstance().getPersistenceController();
-        List<Product> products = persistenceController.findAll(Product.class);
-
-        for (Product p : products) {
-            VBox productBox  = new VBox(10);
-            productBox.setPadding(new Insets(15));
-            productBox.setAlignment(Pos.CENTER);
-            productBox.setPrefSize(200, 170);
-            productBox.setSpacing(10);
-            productBox.setStyle(
-                "-fx-border-color: #DADADA;" + "-fx-border-width: 1;" +
-                "-fx-border-radius: 8;" + "-fx-background-radius: 8;"
-            );
-
-            Label name = new Label(p.getName());
-            name.setMaxWidth(Double.MAX_VALUE);
-            name.setStyle(
-                "-fx-alignment: center;" + "-fx-font-weight: bold;" + "-fx-font-size: 14;"
-            );
-
-            Text description = new Text(p.getDescription());
-            description.setWrappingWidth(180);
-
-            Label unitPrice = new Label("R$ " + p.getUnitPrice());
-            unitPrice.setStyle("-fx-font-weight: bold;");
-
-            Button addCart = new Button("Adicionar ao Carrinho");
-            addCart.setStyle(
-                "-fx-background-color: #1976D2;" + "-fx-text-fill: white;" +
-                "-fx-background-radius: 6;" + "-fx-cursor: hand;"
-            );
-            
-            productBox.getChildren().addAll(name, description, unitPrice, addCart);
-
-            catalog.getChildren().add(productBox);
-        }
-
-        VBox root = new VBox(15);
-        root.setPadding(new Insets(15));
-
-        root.getChildren().addAll(title, catalog);
-
-        ScrollPane scroll = new ScrollPane(root);
-        scroll.setFitToWidth(true);
-
-        IUIController uiController = ICore.getInstance().getUIController();
-        uiController.createTab("Catalog", scroll);
-    }
-
     public boolean init() {
-        ProductListing();
+        CatalogView view = new CatalogView();
+        view.show();
 
         return true;
     }
---
+++ microkernel/plugins/catalogplugin/src/main/java/br/edu/ifba/inf008/plugins/CatalogView.java
@@ -0,0 +1,188 @@
+package br.edu.ifba.inf008.plugins;
+
+import br.edu.ifba.inf008.interfaces.ICore;
+import br.edu.ifba.inf008.interfaces.IUIController;
+import br.edu.ifba.inf008.interfaces.IPersistenceController;
+import br.edu.ifba.inf008.domain.Product;
+
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.geometry.Insets;
+import javafx.scene.text.Text;
+import java.math.BigDecimal;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import java.util.List;
+
+public class CatalogView
+{
+    private FlowPane catalog;
+    private IPersistenceController persistenceController = ICore.getInstance().getPersistenceController();
+
+    private void loadProducts() {
+        catalog.getChildren().clear();
+
+        List<Product> products = persistenceController.findAll(Product.class);
+
+        for (Product p : products) {
+
+            VBox productBox = new VBox(10);
+            productBox.setPadding(new Insets(15));
+            productBox.setAlignment(Pos.CENTER);
+            productBox.setPrefSize(200, 170);
+            productBox.setSpacing(10);
+
+            productBox.setStyle(
+                "-fx-border-color: #DADADA;" + "-fx-border-width: 1;" + 
+                "-fx-border-radius: 8;" + "-fx-background-radius: 8;"
+            );
+
+            Label name = new Label(p.getName());
+            name.setMaxWidth(Double.MAX_VALUE);
+            name.setStyle(
+                "-fx-alignment: center;" + "-fx-font-weight: bold;" + "-fx-font-size: 14;"
+            );
+
+            Text description = new Text(p.getDescription());
+            description.setWrappingWidth(180);
+
+            Label unitPrice = new Label("R$ " + p.getUnitPrice());
+            unitPrice.setStyle("-fx-font-weight: bold;");
+
+            Button addCart = new Button("Adicionar ao Carrinho");
+            addCart.setStyle(
+                "-fx-background-color: #1976D2;" + "-fx-text-fill: white;" +
+                "-fx-background-radius: 6;" + "-fx-cursor: hand;"
+            );
+
+            productBox.getChildren().addAll(name, description, unitPrice, addCart);
+
+            catalog.getChildren().add(productBox);
+        }
+    }
+
+    //Cadastro de produtos
+    private StackPane createForm() {
+        Label title = new Label("Cadastro de Produto");
+        title.setStyle("-fx-font-size: 24px;" + "-fx-font-weight: bold;");
+
+        VBox form = new VBox(15);
+
+        String fieldStyle = 
+            "-fx-background-radius: 6;" + "-fx-border-radius: 6;" +
+            "-fx-border-color: #CCCCCC;" + "-fx-font-size: 14px;"
+        ;
+
+        Label lblSku = new Label("SKU");
+        TextField txtSku = new TextField();
+        txtSku.setPromptText("Ex: SKU-01");
+        txtSku.setMaxWidth(Double.MAX_VALUE);
+        txtSku.setStyle(fieldStyle);
+
+        Label lblName = new Label("Nome");
+        TextField txtName = new TextField();
+        txtName.setPromptText("Nome");
+        txtName.setMaxWidth(Double.MAX_VALUE);
+        txtName.setStyle(fieldStyle);
+        
+        Label lblDescription = new Label("Descrição");
+        TextArea txtDescription = new TextArea();
+        txtDescription.setPromptText("Descrição...");
+        txtDescription.setMaxWidth(Double.MAX_VALUE);
+        txtDescription.setStyle(fieldStyle);
+        txtDescription.setPrefRowCount(6);
+        
+        Label lblPrice = new Label("Preço");
+        TextField txtPrice = new TextField();
+        txtPrice.setPromptText("0.00");
+        txtPrice.setMaxWidth(Double.MAX_VALUE);
+        txtPrice.setStyle(fieldStyle);
+
+        VBox skuField = new VBox(5, lblSku, txtSku);
+        VBox nameField = new VBox(5, lblName, txtName);
+        VBox descriptionField = new VBox(5, lblDescription, txtDescription);
+        VBox PriceField = new VBox(5, lblPrice, txtPrice);
+
+        form.getChildren().addAll(skuField, nameField, descriptionField, PriceField);
+
+        Button save = new Button("Salvar");
+        save.setStyle(
+            "-fx-background-color: #1976D2;" + "-fx-text-fill: white;" + 
+            "-fx-font-weight: bold;" + "-fx-padding: 8 22;" + "-fx-background-radius: 6;" +
+            "-fx-cursor: hand;"
+        );
+
+        save.setOnAction(event -> {
+            String sku = txtSku.getText();
+            String name = txtName.getText();
+            String description = txtDescription.getText();
+            BigDecimal price = new BigDecimal(txtPrice.getText());
+
+            Product product = new Product(sku, name, description, price);
+            persistenceController.save(product);
+
+            loadProducts();
+
+            txtSku.clear();
+            txtName.clear();
+            txtDescription.clear();
+            txtPrice.clear();
+        });
+
+        HBox button = new HBox(10, save);
+        button.setAlignment(Pos.CENTER_RIGHT);
+        
+        VBox card = new VBox(15);
+        card.setMaxWidth(650);
+        card.setPadding(new Insets(30));
+        card.setStyle(
+            "-fx-background-radius: 10;" + "-fx-border-color: #DDDDDD;" + 
+            "-fx-border-radius: 10;"
+        );
+
+        card.getChildren().addAll(title, form, button);
+
+        StackPane root = new StackPane(card);
+        root.setPadding(new Insets(30));
+        root.setAlignment(Pos.TOP_CENTER);
+        root.setStyle("-fx-background-color: #F5F5F5;");
+
+        return root;
+    }
+
+    //Listagem de produtos
+    private ScrollPane createCatalog() {
+        Label title = new Label("Catálogo de Produtos");
+        title.setStyle("-fx-font-size: 24px;" + "-fx-font-weight: bold;");
+
+        catalog = new FlowPane();
+        catalog.setHgap(15);
+        catalog.setVgap(15);
+
+        loadProducts();
+
+        VBox root = new VBox(15);
+        root.setPadding(new Insets(15));
+
+        root.getChildren().addAll(title, catalog);
+
+        ScrollPane scroll = new ScrollPane(root);
+        scroll.setFitToWidth(true);
+
+        return scroll;
+    }
+
+    public void show() {
+        IUIController uiController = ICore.getInstance().getUIController();
+        uiController.createTab("Catalog", createCatalog());
+        uiController.createTab("Form", createForm());
+    }
+    
+}
