3b7c4ce385c16721df9074ab66717ed7b11dcc5e
VanessaVilas <vanessavilas57@gmail.com>
2026-07-18 17:27:37 -0300

    converting InsufficientStockException into a RuntimeException

--- microkernel/plugins/catalogplugin/src/main/java/br/edu/ifba/inf008/plugins/InsufficientStockException.java
+++ microkernel/plugins/catalogplugin/src/main/java/br/edu/ifba/inf008/plugins/InsufficientStockException.java
@@ -1,6 +1,6 @@
 package br.edu.ifba.inf008.plugins;
 
-public class InsufficientStockException extends Exception {
+public class InsufficientStockException extends RuntimeException {
     public InsufficientStockException() {
         super("Estoque insuficiente.");
     }
